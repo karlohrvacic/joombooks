b127e7da0fb001d95ae5428706478600c57795a1
Karlo Hrvačić <34110072+karlohrvacic@users.noreply.github.com>
2021-05-06 22:09:56 +0200

    Test barcodes

--- src/Controller/BarcodeController.php
+++ src/Controller/BarcodeController.php
@@ -5,19 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use CodeItNow\BarcodeBundle\Utils\BarcodeGenerator;
 
-class BarcodeController extends AbstractController
+class BarcodeController
 {
 
-    private $barcode;
 
-    public function makeBarcode($text, $typeExt): BarcodeGenerator
+    public static function makeBarcode($text, $typeExt): BarcodeGenerator
     {
-        $this->barcode = new BarcodeGenerator();
-        $this->barcode->setText($text);
-        $this->barcode->setType(constant('CodeItNow\BarcodeBundle\Utils\BarcodeGenerator::'.$typeExt));
-        $this->barcode->setScale(2);
-        $this->barcode->setThickness(25);
-        $this->barcode->setFontSize(10);
-        return $this->barcode;
+        $barcode = new BarcodeGenerator();
+        $barcode->setText($text);
+        $barcode->setType(constant('CodeItNow\BarcodeBundle\Utils\BarcodeGenerator::'.$typeExt));
+        $barcode->setScale(2);
+        $barcode->setThickness(25);
+        $barcode->setFontSize(10);
+        return $barcode;
     }
 }
