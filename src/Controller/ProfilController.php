2000de09215d1a7f0b155aa7365f755fb48a7b12
Karlo Hrvačić <34110072+karlohrvacic@users.noreply.github.com>
2021-05-06 15:30:37 +0200

    Pls work

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -12,6 +12,7 @@ use DateInterval;
 use DateTime;
 use Exception;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
