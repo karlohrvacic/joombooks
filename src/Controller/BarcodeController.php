2275cb9b45eb369aec69ad28ffe712b7b3420766
Karlo Hrvačić <34110072+karlohrvacic@users.noreply.github.com>
2021-05-06 22:30:36 +0200

    Test barcodes

--- src/Controller/BarcodeController.php
+++ src/Controller/BarcodeController.php
@@ -2,21 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Picqer\Barcode\BarcodeGeneratorPNG;
 use CodeItNow\BarcodeBundle\Utils\BarcodeGenerator;
 
 class BarcodeController
 {
 
-
-    public static function makeBarcode($text, $typeExt): BarcodeGenerator
+    public static function makeBarcode($text, $typeExt): string
     {
-        $barcode = new BarcodeGenerator();
+        /*$barcode = new BarcodeGenerator();
         $barcode->setText($text);
         $barcode->setType(constant('CodeItNow\BarcodeBundle\Utils\BarcodeGenerator::'.$typeExt));
         $barcode->setScale(2);
         $barcode->setThickness(25);
         $barcode->setFontSize(10);
-        return $barcode;
+        return $barcode;*/
+        $redColor = [255, 0, 0];
+
+        $generator = new BarcodeGeneratorPNG();
+        //echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode('081231723897', $generator::TYPE_CODE_128)) . '">';
+
+        return base64_encode($generator->getBarcode("081231723897", $generator::TYPE_CODE_128, 3, 50));
     }
 }
