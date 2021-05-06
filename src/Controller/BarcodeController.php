45fafc05d304e87703c113e163a359f593f5b692
Karlo Hrvačić <34110072+karlohrvacic@users.noreply.github.com>
2021-05-06 23:06:05 +0200

    Barcodes

--- src/Controller/BarcodeController.php
+++ src/Controller/BarcodeController.php
@@ -2,28 +2,23 @@
 
 namespace App\Controller;
 
-use Picqer\Barcode\BarcodeGeneratorPNG;
 use CodeItNow\BarcodeBundle\Utils\BarcodeGenerator;
 
 class BarcodeController
 {
 
-    public static function makeBarcode($text, $typeExt)
+    public static function makeBarcode($text, $typeExt, $colour)
     {
+        $colour == "" ? $colour : "#ffffff";
+
         $barcode = new BarcodeGenerator();
         $barcode->setText($text);
         $barcode->setType(constant('CodeItNow\BarcodeBundle\Utils\BarcodeGenerator::'.$typeExt));
         $barcode->setScale(2);
         $barcode->setThickness(25);
         $barcode->setFontSize(10);
+        $barcode->setBackgroundColor($colour);
 
         return $barcode;
-
-        $redColor = [255, 0, 0];
-
-        $generator = new BarcodeGeneratorPNG();
-        //echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode('081231723897', $generator::TYPE_CODE_128)) . '">';
-
-        return base64_encode($generator->getBarcode("081231723897", $generator::TYPE_CODE_128, 3, 50));
     }
 }
