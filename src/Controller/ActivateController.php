b3ecd345d6ce0f91af48d2779c91b6298936470f
Karlo Hrvačić <34110072+karlohrvacic@users.noreply.github.com>
2021-05-09 18:33:26 +0200

    Removed bar from activation

--- src/Controller/ActivateController.php
+++ src/Controller/ActivateController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Korisnici;
 use App\Form\ActivationType;
-use MercurySeries\FlashyBundle\FlashyNotifier;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
