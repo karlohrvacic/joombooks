11a600d7b5ded3213aaab9bb2b8daa24e47774d8
panda-art <dinterjulius@gmail.com>
2021-05-10 13:55:02 +0200

    knjiznica editing

--- src/Form/KorisniciType.php
+++ src/Form/KorisniciType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,22 +17,71 @@ class KorisniciType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('ime')
-            ->add('prezime')
-            ->add('email', EmailType::class)
+            ->add('ime', TextType::class, [
+                'row_attr' => [
+                    'class' => 'knjiznica-label'
+                ],
+                'attr' => [
+                        'class' => 'form-control'
+            ]])
+            ->add('prezime', TextType::class, [
+                'row_attr' => [
+                    'class' => 'knjiznica-label'
+                ],
+                'attr' => [
+                    'class' => 'form-control'
+                ]])
+            ->add('email', EmailType::class, [
+                'row_attr' => [
+                        'class' => 'knjiznica-label'
+                ],
+                'attr' => [
+                        'class' => 'form-control'
+                ]
+            ])
+
+
             /*->add('lozinka', PasswordType::class, [
                 'required' => false,
                 'empty_data' => ''
             ])*/
-                ->add('brojIskazniceKorisnika', NumberType::class)
+                ->add('brojIskazniceKorisnika', NumberType::class,[
+                    'row_attr' => [
+                        'class' => 'knjiznica-label'
+                    ],
+                    'attr' => [
+                        'class' => 'form-control'
+                    ]
+                ]
+
+            )
             ->add('brojTelefona', NumberType::class, [
                 'required'   => false,
+                'row_attr' => [
+                    'class' => 'knjiznica-label'
+                ],
+                'attr' => [
+                    'class' => 'form-control'
+                ]
                 ])
             ->add('fotografija', FileType::class, [
                 'mapped' => false,
                 'required' => false,
+                'row_attr' => [
+                    'class' => 'knjiznica-label'
+                ],
+                'attr' => [
+                    'class' => 'form-control'
+                ]
             ])
-            ->add('razred')
+            ->add('razred', TextType::class,[
+                'row_attr' => [
+        'class' => 'knjiznica-label'
+    ],
+                'attr' => [
+        'class' => 'form-control'
+    ]
+            ])
         ;
     }
 
