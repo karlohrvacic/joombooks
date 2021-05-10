dc46d81f0f64c9a4e635e9bf163f093ab3c47ce0
Karlo Hrvačić <34110072+karlohrvacic@users.noreply.github.com>
2021-05-10 17:52:41 +0200

    Jezici fix

--- src/Form/JeziciType.php
+++ src/Form/JeziciType.php
@@ -3,8 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Jezici;
-use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
